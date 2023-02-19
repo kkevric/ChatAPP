5500b8404da8e00d545a0a1e693449bd07f3034a
Katja Kevric <katja.kevric@gmail.com>
2023-02-19 11:45:45 +0100

    version 1

---
+++ src/Messages.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Messages = ({messages, currentMember}) =>{
+
+    const renderMessage = (message) => {
+      const {member, text} = message;
+      const messageFromMe = member.id === currentMember.id;
+      const className = messageFromMe ?
+        "Messages-message currentMember" : "Messages-message";
+      return (
+        <li className={className}>
+          <span
+            className="avatar"
+            style={{backgroundColor: member.clientData.color}}
+          />
+          <div className="Message-content">
+            <div className="username">
+              {member.clientData.username}
+            </div>
+            <div className="text">{text}</div>
+          </div>
+        </li>
+      );
+    }
+
+    
+
+    return (
+      <ul className="Messages-list">
+        {messages.map((message) => renderMessage(message))}
+      </ul>
+    );
+
+      
+ 
+}
+
+
+
+export default Messages;
